41b9c08b815350054b2cfcd1ef4ba17cd5abf350
DevRaf4elSantos <contatodevrafaelsantos@gmail.com>
2025-05-17 18:03:15 -0300

    fix: Adicionando caminho principal da apllicação para teste'

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended : true}));
 
+app.get('/', (req : Request, res : Response) => {
+    res.status(200).send('Seja bem-vind@');
+});
+
 app.listen(process.env.PORT, ()=>{
     console.log("Sistema Rodando com sucesso");
     
